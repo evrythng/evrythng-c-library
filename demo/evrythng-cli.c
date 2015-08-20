9513a293ef9edde8866d9f16fbf10db521bee0c4
Ruslan Filipenko <ruslan.filipenko@evrythng.com>
2015-08-20 17:08:10 +0300

    add source headers

--- demo/evrythng-cli.c
+++ demo/evrythng-cli.c
@@ -1,3 +1,8 @@
+/*
+ * (c) Copyright 2012 EVRYTHNG Ltd London / Zurich
+ * www.evrythng.com
+ */
+
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
--- evrythng/include/platform.h
+++ evrythng/include/platform.h
@@ -1,3 +1,8 @@
+/*
+ * (c) Copyright 2012 EVRYTHNG Ltd London / Zurich
+ * www.evrythng.com
+ */
+
 /**
  *
  * @file   platform.h
