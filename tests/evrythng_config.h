0247662cd6aaad1158e2fbbb05987e6be07053a6
Ruslan Filipenko <ruslan.filipenko@evrythng.com>
2015-08-28 16:28:27 +0300

    added config.h for tests

---
+++ tests/evrythng_config.h
@@ -0,0 +1,13 @@
+#ifndef EVT_CONFIG_H
+#define EVT_CONFIG_H
+
+#define MQTT_URL "ssl://mqtt.evrythng.com:443"
+#define DEVICE_API_KEY "HiX0xYZwULxR0GBWb9ZuQi8vTcPSndRxfnx9iIvw4u12Bdt6iMxkjwXujCkadQfBfTiV7kGLx80JPdGj"
+#define THNG_1 "UfFcGftssBpwrSQ8bmT7Ammr"
+#define PRODUCT_1 "UfkcGeahPepabCk5dNdBBnQr"
+#define PROPERTY_1 "property_1"
+#define PROPERTY_2 "property_2"
+#define ACTION_1 "_action_1"
+#define ACTION_2 "_action_2"
+
+#endif
