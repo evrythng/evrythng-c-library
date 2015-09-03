ec6a47208809d32c268bad49d337b224f0d3666a
Ruslan Filipenko <ruslan.filipenko@evrythng.com>
2015-09-03 13:23:27 +0300

    config changes

--- tests/evrythng_config.h
+++ tests/evrythng_config.h
@@ -3,6 +3,7 @@
 
 #define MQTT_URL "ssl://mqtt.evrythng.com:443"
 #define DEVICE_API_KEY "HiX0xYZwULxR0GBWb9ZuQi8vTcPSndRxfnx9iIvw4u12Bdt6iMxkjwXujCkadQfBfTiV7kGLx80JPdGj"
+#define DEVICE_API_KEY_BACKUP "Z5KFS8LOsart7lehgQRtgV1rHXmMwy7Sw5grpJgkdhqmbM2uxej0hZEiw85zPZOQ6akapoTvDaYWJ7px"
 #define THNG_1 "UfFcGftssBpwrSQ8bmT7Ammr"
 #define PRODUCT_1 "UfkcGeahPepabCk5dNdBBnQr"
 #define PROPERTY_1 "property_1"
