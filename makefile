b08bf4dda71271d3514e531a95ec494824efb775
Ruslan Filipenko <ruslan.filipenko@evrythng.com>
2017-02-28 14:44:18 +0300

    added params to gen_header script and removed h from source control

--- makefile
+++ makefile
@@ -3,7 +3,7 @@
 RMRF=rm -rf
 PROJECT_DIR=$(shell pwd)
 
-all: gen_config docs
+all: docs
 
 clean:
 	@$(RMRF) docs/html
@@ -12,4 +12,4 @@ docs:
 	@doxygen docs/Doxyfile
 
 gen_config:
-	@$(PROJECT_DIR)/tests/gen_header.sh
+	@$(PROJECT_DIR)/tests/gen_header.sh $(PROJECT_DIR)/Config $(PROJECT_DIR)/tests/evrythng_config.h
--- tests/gen_header.sh
+++ tests/gen_header.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
-CONFIG_FILE=Config
-HEADER_FILE=tests/evrythng_config.h
+CONFIG_FILE=$1
+HEADER_FILE=$2
 
+if [ "$1" != "" ]; then
+    echo "Using test configuration file: ${CONFIG_FILE}"
+else
+    echo "a path to configuration config was not provided"
+    exit
+fi
+
+if [ "$2" != "" ]; then
+    echo "Generating configuration header: ${HEADER_FILE}"
+else
+    echo "a path to configuration header was not provided"
+    exit
+fi
 
 if [ ! -f ${CONFIG_FILE} ]
 then
     echo "config file was not found"
     exit
 fi
-
 
 : > ${HEADER_FILE}
 
